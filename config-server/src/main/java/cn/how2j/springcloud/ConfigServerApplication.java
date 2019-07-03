dcd191bcdf3d2dd0dac51f6e745f3708ba8a7897
litianqi <litianqi@hikvision.com>
2019-07-03 13:53:15 +0800

    新加config-server模块

---
+++ config-server/src/main/java/cn/how2j/springcloud/ConfigServerApplication.java
@@ -0,0 +1,24 @@
+package cn.how2j.springcloud;
+
+
+import cn.hutool.core.util.NetUtil;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication{
+    public static void main(String[] args) {
+        int port = 8030;
+        if (!NetUtil.isUsableLocalPort(port)){
+            System.out.printf("端口%d已被占用,无法启动%n",port);
+            System.exit(1);
+        }
+        new SpringApplicationBuilder(ConfigServerApplication.class).properties("server.port:"+port).run(args);
+    }
+}
